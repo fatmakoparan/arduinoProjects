c96f4bde4d2cffb00ef458a44108b957645c0576
Fatma Koparan <97118786+fatmakoparan@users.noreply.github.com>
2024-01-29 17:23:21 +0300

    Second commit with Running Light Circuit
    - Added the Running Light Circuit code and circuit diagram.
    - Updated the README.md with information about the Running Light Circuit.

---
+++ running_light_circuit.ino
@@ -0,0 +1,25 @@
+const int sure = 79;
+
+void setup() {
+ for(int ledPin=2; ledPin<12;ledPin++)
+ {
+    pinMode(ledPin,OUTPUT);
+ }
+}
+
+void loop() {
+  for(int ledPin=2; ledPin<12;ledPin++)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(sure);
+    digitalWrite(ledPin,LOW);
+
+  }
+  for(int ledPin=12; ledPin>1;ledPin--)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(sure);
+    digitalWrite(ledPin,LOW);
+    
+  }
+}
