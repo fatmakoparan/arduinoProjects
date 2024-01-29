4f3e193055d3b29f9150d22f1a48695e718519da
Fatma Koparan <97118786+fatmakoparan@users.noreply.github.com>
2024-01-29 17:41:43 +0300

    buttons and lights
    the lamp flashes when you press one button and goes out with the other button

---
+++ buttons_and_light.ino
@@ -0,0 +1,19 @@
+const int ledPin=9;
+const int buttonPin=10;
+const int buttonPin2=11;
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+  pinMode(buttonPin2,INPUT);
+}
+
+void loop() {
+   int deger=digitalRead(buttonPin);
+   int deger2=digitalRead(buttonPin2);
+   if(deger==HIGH)
+    digitalWrite(ledPin,HIGH);
+   else if(deger2==HIGH)
+    digitalWrite(ledPin,LOW);
+}
+ //bu devrede bir tusa basıyorken aciliyor diger tusa basilinca kapaniyor
+ //fakat ayni tusla acma ve kapama mevcut degil 
