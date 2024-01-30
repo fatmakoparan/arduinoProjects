f33a5c1eb5a3e40b028d8eecda72943339554b7f
Fatma Koparan <97118786+fatmakoparan@users.noreply.github.com>
2024-01-30 10:02:55 +0300

    The light that burns in the dark

---
+++ the_light_that_burns_in_the_dark.ino
@@ -0,0 +1,13 @@
+const int ledPin=9;
+const int LDRpin=A0;
+void setup() {
+  pinMode(ledPin,OUTPUT);  
+}
+
+void loop() {
+  int deger =analogRead(LDRpin);
+  if(deger<900)
+   digitalWrite(ledPin,HIGH);
+   else
+   digitalWrite(ledPin,LOW);
+}
