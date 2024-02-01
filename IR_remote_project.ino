570c464d8b866e725f68908798f0f334beedf81f
Fatma Koparan <97118786+fatmakoparan@users.noreply.github.com>
2024-02-01 12:00:31 +0300

    IR remote project
    This code detects the keystrokes of an IR remote control using an infrared (IR) receiver and adjusts the colors of an RGB LED (red, green, blue) based on these keystrokes. A different control key is assigned for each color; when certain keys are pressed, the corresponding color value increases or decreases, and one key resets the LED. The code shows which key was pressed by printing the HEX code of the detected IR signal via the serial port.

---
+++ IR_remote_project.ino
@@ -0,0 +1,47 @@
+#include <IRemote.h>
+
+int sensorPin=2;
+int redPin=5;
+int greenPin=6;
+int bluePin=9;
+
+byte red,green,blue;
+
+IRrecv sensor(sensorPin);
+decode_results deger;
+void setup() {
+ sensor.enableIRIn();
+ pinMode (redPin,OUTPUT);
+ pinMode (redPin,OUTPUT);
+ pinMode (redPin,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+   if(sensor.decode(&deger))
+   {
+    Serial.print("Tuş kodu: ");
+    Serial.prinln(deger.value, HEX);
+    sensor.resume();
+
+   if(deger.value == 0xFFA25D)
+     red=red+32;
+   if(deger.value == 0xFF629D)
+     red=red-32;
+   if(deger.value== 0xFF22DD)
+     green == green+32;
+   if(deger.value == 0xFF02FD)
+     green == green-32;
+   if(deger.value == 0xFFE01F)
+     blue=blue+32;
+   if(deger.value == 0xFFA857)
+     blue=blue-32;
+   if(deger.value == 0xFF38C7)
+     blue=0,green=0,red=0;
+     analogWrite(redPin,red);
+     analogWrite(greenPin,green);
+     analogWrite(bluePin,blue);
+    
+   }
+   delay(10);
+}
